8346fcb5b0f2c548a3cc850a7a1dab7a714a2599
sfaye <m@sfaye.com>
2015-10-01 14:38:03 +0200

    web-service #1
    First commit

---
+++ service.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ *                         SWIPE Web Service
+ *                Author: Sébastien FAYE [sebastien.faye@uni.lu]
+ *
+ * ------------------------------------------------------------------------------
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Sébastien FAYE [sebastien.faye@uni.lu], VehicularLab [vehicular-lab@uni.lu]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * ------------------------------------------------------------------------------
+ */
+ 
+function human_filesize($bytes, $decimals = 2) {
+  $sz = 'BKMGTP';
+  $factor = floor((strlen($bytes) - 1) / 3);
+  return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+}
+
+$dossier = opendir("./files");
+$i = 100;
+$tab = array();
+while (($file = readdir($dossier)) !== false) {
+	if(preg_match("#main#i", $file)) {
+		$tab[preg_replace("#^[^_]+_main_[a-z0-9]+_([0-9]+)$#i", "$1", $file).$i] = $file.":".human_filesize(filesize("./files/".$file));
+		$i++;
+	}
+}
+krsort($tab);
+echo "{";
+$i = 0;
+foreach($tab as $v) {
+	if($i > 0)
+		echo ";";
+	echo $v;
+	$i++;
+}
+echo "}";
+
+closedir($dossier);
+?>
